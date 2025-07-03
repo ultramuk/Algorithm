9cf59a0396baf14faf5431c36ab7a95bf0fffff4
KyeongmukLim <67509269+ultramuk@users.noreply.github.com>
2025-07-03 12:59:53 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    return accumulate(arr.begin(), arr.end(), 0.0) / arr.size();
+}
