77932c99c9ae73b8da505f50bed69b07c62bc1d5
KyeongmukLim <67509269+ultramuk@users.noreply.github.com>
2025-07-03 13:02:35 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    int num = x;
+    int sum = 0;
+    
+    while(num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    
+    if(x % sum)
+        return false;
+    return true;
+}
