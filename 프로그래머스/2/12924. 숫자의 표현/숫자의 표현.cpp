8567ab135d74e553fb15198ddaf3d5e3ac10a207
KyeongmukLim <67509269+ultramuk@users.noreply.github.com>
2025-07-07 09:42:44 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 3.75 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int count = 1;
+
+    for(int i=1; i<n/2+1; i++) {
+        int sum = i, j = i;
+
+        while(n >= sum) {
+            sum += ++j;
+            if(sum == n)
+                count++;
+        }
+    }
+    
+    return count;
+}
