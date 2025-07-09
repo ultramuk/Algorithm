72e1b55507ea66c6b35d14e2aa9d1b58bd157aeb
KyeongmukLim <67509269+ultramuk@users.noreply.github.com>
2025-07-09 09:38:28 +0900

    [level 2] Title: 피보나치 수, Time: 0.59 ms, Memory: 4.29 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int fibo[100001];
+
+int solution(int n) {
+    fibo[0] = 0;
+    fibo[1] = 1;
+    
+    for(int i=2; i<=n; ++i) {
+        fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
+    }
+    
+    return fibo[n];
+}
