2b21d4bc9e9d374998b822d98d00a3723d07f836
KyeongmukLim <67509269+ultramuk@users.noreply.github.com>
2025-07-16 09:49:44 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.06 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -7,9 +7,7 @@ int N;
 vector<string> solution(vector<string> strings, int n) {
     N = n;
     sort(strings.begin(), strings.end(), [](string s1, string s2){
-        if(s1[N] == s2[N])
-            return s1 < s2;
-        return s1[N] < s2[N];
+        return s1[N] == s2[N] ? s1 < s2 : s1[N] < s2[N];
     });   
     
     return strings;
