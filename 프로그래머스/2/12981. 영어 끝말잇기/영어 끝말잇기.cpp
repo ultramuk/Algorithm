8e712190cdc719c2ad872a61e5af0314cd3f2022
KyeongmukLim <67509269+ultramuk@users.noreply.github.com>
2025-07-21 08:42:27 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
+
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
@@ -12,15 +12,13 @@ vector<int> solution(int n, vector<string> words) {
     for(int i=1; i<words.size(); i++) {
         // 끝말잇기 조건 체크
         if(words[i][0] != ch) {
-            cout << i+1;
-            return {((i+1)%n != 0 ? (i+1)%n : n), i/n+1};
+            return {i%n+1, i/n+1};
         }
         ch = words[i][words[i].length()-1];
         
         // 중복 체크
         if(find(before_words.begin(), before_words.end(), words[i]) != before_words.end()) {
-            cout << i+1;
-            return {((i+1)%n != 0 ? (i+1)%n : n), i/n+1};
+            return {i%n+1, i/n+1};
         }
         before_words.push_back(words[i]);
     }
