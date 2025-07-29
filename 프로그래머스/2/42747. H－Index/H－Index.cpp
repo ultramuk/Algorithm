8b3e9744b527e96e6f45165a6b13d3abf6d093ce
KyeongmukLim <67509269+ultramuk@users.noreply.github.com>
2025-07-30 08:59:06 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int solution(vector<int> citations) {
-    for(int h = 1000; h > 0; h--){
-        int num = 0;
-        for(int i = 0; i < citations.size(); i++) {
-            if (citations[i] >= h) num++;
-        }
-        if (num >= h) return h;
+    for(int answer=1000; answer>=0; answer--) {
+        int count = 0;
+        for(auto c : citations)
+            if(answer <= c) count++;
+        
+        if(count >= answer) return answer;
     }
     
     return 0;
