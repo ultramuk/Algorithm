43b55d8507fa2217ad7c7b084a9d5249158e3c5d
KyeongmukLim <67509269+ultramuk@users.noreply.github.com>
2025-08-04 08:54:11 +0900

    [level 2] Title: 피로도, Time: 0.79 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -4,34 +4,30 @@
 
 using namespace std;
 
-int solution(int k, vector<vector<int>> dungeons) {
-    // step 1
-    vector<int> v;    
-    for(int i=0; i<dungeons.size(); i++)
-        v.push_back(i);
+int answer = 0;
+vector<vector<int>> D;
+int N;
+bool visited[8] = {false};
+
+void dfs(int n, int p) {
+    if(answer < n)
+        answer = n;
     
-    // step 2
-    vector<vector<int>> vec;
-    do {
-        vector<int> temp;
-        for(auto it = v.begin(); it != v.end(); it++)
-            temp.push_back(*it);
-        vec.push_back(temp);
-    } while(next_permutation(v.begin(), v.end()));
-    
-    // step 3
-    int answer = -1;
-    for(auto& ve : vec) {
-        int kk = k;
+    for(int i=0; i<N; i++) {
+        if(visited[i] || p < D[i][0])
+            continue;
         
-        int ans = 0;
-        for(int i=0; i<ve.size(); i++) { 
-            auto j = ve[i];
-            if(dungeons[j][0] > kk) break;
-            kk -= dungeons[j][1];
-            ans++;
-        }
-        answer = max(answer, ans);
+        visited[i] = true;
+        dfs(n+1, p-D[i][1]);
+        visited[i] = false;
     }
+}
+
+int solution(int k, vector<vector<int>> dungeons) {
+    D = dungeons;
+    N = dungeons.size();
+    
+    dfs(0, k);
+    
     return answer;
 }
