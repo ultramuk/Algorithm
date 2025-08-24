1e50520dab521a0b3947e2fffec4a34cbd4aeb80
KyeongmukLim <67509269+ultramuk@users.noreply.github.com>
2025-08-25 08:01:58 +0900

    [level 2] Title: 더 맵게, Time: 211.67 ms, Memory: 39.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    for(auto s : scoville)
+        pq.push(s);
+    
+    int answer = 0;
+    while(pq.top() < K) {
+        if(pq.size() < 2)
+            return -1;
+        
+        auto first = pq.top(); pq.pop();
+        auto second = pq.top(); pq.pop();
+        
+        pq.push(first+2*second);
+        answer++;
+    }
+    
+    return answer;
+}
