a92acfbe22e4c372d51794b296045b8aeefa8115
KyeongmukLim <67509269+ultramuk@users.noreply.github.com>
2025-09-02 08:20:41 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
@@ -8,14 +8,14 @@ int solution(string skill, vector<string> skill_trees) {
     
     for(auto& skill_tree : skill_trees) {
         bool is_skill_tree = true;
-        int i = 0;
+        int skill_index = 0;
         
         for(auto sk : skill_tree) {
             if(skill.find(sk) == string::npos)
                 continue;
             
-            if(skill.find(sk) == i) {
-                i++;
+            if(skill.find(sk) == skill_index) {
+                skill_index++;
             }
             else {
                 is_skill_tree = false;
